af0350428665a7e9b9c233172639985e415a2474
Konstantin <kostyantyn.tatarnikov@gmail.com>
2025-12-26 13:02:27 +0100

    version 25.06

--- vpp-api-client/src/lib.rs
+++ vpp-api-client/src/lib.rs
@@ -4,3 +4,10 @@ pub mod client;
 
 #[cfg(feature = "version_25_10")]
 pub use client::*;
+
+#[cfg(feature = "version_25_06")]
+#[path = "../gen/25.06/src/mod.rs"]
+pub mod client;
+
+#[cfg(feature = "version_25_06")]
+pub use client::*;
