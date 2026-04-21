fa92b7513950d2f835d46267f3700bff34d9ebda
Kostyantyn Tatarnikov <kostyantyn.tatarnikov@gmail.com>
2026-04-21 13:35:56 +0200

    vpp-api 26.02

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Rust VPP API
 
 This is a place-holder for now, while the lower layers are built.
 If you are interested - check out vpp-api-transport crate.
+
+
+sudo apt install build-essentials clang
--- vpp-api-client/src/lib.rs
+++ vpp-api-client/src/lib.rs
@@ -1,7 +1,34 @@
 use cfg_if::cfg_if;
 
 cfg_if! {
-    if #[cfg(feature = "25_10")] {
+    if #[cfg(feature = "26_02")] {
+
+        #[path = "../gen/26.02/src/mod.rs"]
+        #[rustfmt::skip]
+        #[allow(clippy::all)]
+        pub mod client;
+
+        pub use client::*;
+
+        #[cfg(test)]
+        #[path = "../gen/26.02/tests/afunix_interface_test.rs"]
+        #[rustfmt::skip]
+        #[allow(clippy::all)]
+        pub mod tests_afunix;
+
+        #[cfg(test)]
+        #[path = "../gen/26.02/tests/blocking_interface_test.rs"]
+        #[rustfmt::skip]
+        #[allow(clippy::all)]
+        pub mod tests_blocking;
+
+        #[cfg(test)]
+        #[path = "../gen/26.02/tests/nonblocking_interface_test.rs"]
+        #[rustfmt::skip]
+        #[allow(clippy::all)]
+        pub mod tests_nonblocking;
+
+    } else if #[cfg(feature = "25_10")] {
 
         #[path = "../gen/25.10/src/mod.rs"]
         #[rustfmt::skip]
@@ -55,6 +82,6 @@ cfg_if! {
         #[allow(clippy::all)]
         pub mod tests_nonblocking;
     } else {
-        compile_error!("You must enable exactly one version feature: e.g. `25_10` or `25_06`");
+        compile_error!("You must enable exactly one version feature: e.g. `26_02`, `25_10`, or `25_06`");
     }
 }
